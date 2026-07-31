15337346eb47f9935924027e900971a23e222db5
Nathan Blondeau <nathan.blondeau@etu.univ-nantes.fr>
2026-07-31 16:28:17 +0200

    feat (23-Création-partie) service avancement

--- backend/src/main/java/roivivant/Services/PartieService.java
+++ backend/src/main/java/roivivant/Services/PartieService.java
@@ -1,13 +1,47 @@
 package roivivant.Services;
 
 import org.springframework.stereotype.Service;
+import roivivant.Models.Carte;
+import roivivant.Models.Joueur;
 import roivivant.Models.Partie;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class PartieService {
 
-    public Partie initPartie (int nbJoueurs){}
+    public Partie initPartie (ArrayList<Joueur> Joueurs){
+        CarteService carteService = new CarteService();
+        ArrayList<String> enseignes = new ArrayList<>();
+        enseignes.add("coeur");
+        enseignes.add("piques");
+        enseignes.add("trefle");
+        enseignes.add("carreau");
+        List<Carte> pile = carteService.generePool(40, enseignes);
 
-    public void
+        return new Partie(Joueurs,pile);
+    }
+
+    public String jouerUnTour(){
+
+    }
+
+    public String jouerDevinerFaction(){
+
+    }
+
+    public String jouerComparer(){
+
+    }
+
+    public String jouerTuer(){
+
+    }
+
+    public String jouerRegarderSaCarte(){
+
+    }
 
 }
