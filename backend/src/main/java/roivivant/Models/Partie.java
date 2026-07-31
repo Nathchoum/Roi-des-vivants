84ac4d26c87a88908fe55c4719fe157c1e86ec0a
Nathan Blondeau <nathan.blondeau@etu.univ-nantes.fr>
2026-07-31 16:00:58 +0200

    feat (23-Création-partie) model et service pour partie

---
+++ backend/src/main/java/roivivant/Models/Partie.java
@@ -0,0 +1,28 @@
+package roivivant.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Data
+public class Partie {
+    private UUID idPartie;
+    private String cycle;
+    private List<Joueur> joueurs;
+    private List<Carte> pile;
+    private int tours;
+    private List<Integer> ordre;
+
+    // Constructeur pratique pour démarrer une nouvelle partie
+    public Partie(List<Joueur> joueurs, List<Carte> pile) {
+        this.idPartie = UUID.randomUUID(); // Génère l'UUID automatiquement
+        this.cycle = "JOUR";
+        this.joueurs = joueurs;
+        this.pile = pile;
+        this.tours = 1;
+    }
+}
---
+++ backend/src/main/java/roivivant/Services/PartieService.java
@@ -0,0 +1,13 @@
+package roivivant.Services;
+
+import org.springframework.stereotype.Service;
+import roivivant.Models.Partie;
+
+@Service
+public class PartieService {
+
+    public Partie initPartie (int nbJoueurs){}
+
+    public void
+
+}
